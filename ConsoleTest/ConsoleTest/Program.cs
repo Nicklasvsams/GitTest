418e13661687453086a2014680fa7e6605aa33c1
Nicklas Sams <nicklasvsams@gmail.com>
2020-11-11 12:45:48 +0100

    Testcommit

---
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PortLibrary;
+
+namespace ConsoleTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyClass Test_icles = new MyClass("Henning", 15);
+
+            Console.WriteLine(Test_icles.Age + " " + Test_icles.Name);
+        }
+    }
+}
---
+++ ConsoleTest/Port-Library/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PortLibrary
+{
+    public class MyClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public MyClass(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
